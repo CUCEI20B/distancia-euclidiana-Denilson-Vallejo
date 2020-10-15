700e9571499e7906a4513a870c888df50e8c98f9
Denilson Asael Sandoval Vallejo <denilson.sandoval4971@alumnos.udg.mx>
2020-10-15 17:31:02 -0500

    Prueba 3

--- algoritmos.py
+++ algoritmos.py
@@ -1,6 +1,9 @@
 #prueba 3
 import math
-def distancia_euclidiana(x_1, x_2, y_1, y_2):
+def distancia_euclidiana(x_1, y_1, x_2, y_2):
     return math.sqrt((x_2 - x_1)**2+(y_2-y_1)**2)
+
+x = distancia_euclidiana(7, 5, 4, 1)
+print(x)
 
     
